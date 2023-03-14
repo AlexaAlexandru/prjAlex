b504e610a59fe375f095d0cf330249af2aec3a8c
alexandrualexa <liv06ale@gmail.com>
2023-03-14 16:46:33 +0200

    Dynamic Services

--- SchedulePlatform.WebUI/Pages/Services.razor.cs
+++ SchedulePlatform.WebUI/Pages/Services.razor.cs
@@ -1,22 +1,22 @@
 ﻿using System;
 using SchedulePlatform.Models.Entities;
 using Newtonsoft.Json;
-using System.Net.Http.Json;
 using System.Text.Json.Serialization;
+using System.Net.Http.Json;
 
 namespace SchedulePlatform.WebUI.Pages
 {
-	public partial class Services
-	{
-		private List<ServiceProvided> services = new List<ServiceProvided>();
+    public partial class Services
+    {
+        private List<ServiceProvided> services = new List<ServiceProvided>();
 
-		//protected override async Task OnInitializedAsync()
-		//{
-		//	var response = await HttpClient.GetAsync("/api/ServiceProvided");
-		//	var json = await response.Content.ReadAsStringAsync();
+        protected override async Task OnInitializedAsync()
+        {
+            var response = await HttpClient.GetAsync("api/ServiceProvided");
+            var json = await response.Content.ReadAsStringAsync();
 
-		//	services = JsonConvert.DeserializeObject<List<ServiceProvided>>(json);
-		//}
-	}
+            services = JsonConvert.DeserializeObject<List<ServiceProvided>>(json);
+        }
+    }
 }
 
