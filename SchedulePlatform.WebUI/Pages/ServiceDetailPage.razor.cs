6842bffa2b0f21143e1d09ea7ae7d6d1270fb949
alexandrualexa <liv06ale@gmail.com>
2023-03-23 17:47:14 +0200

    From Service to ServiceDetailPage

---
+++ SchedulePlatform.WebUI/Pages/ServiceDetailPage.razor.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using SchedulePlatform.Models.Entities;
+using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace SchedulePlatform.WebUI.Pages
+{
+
+}
--- SchedulePlatform.WebUI/Pages/Services.razor.cs
+++ SchedulePlatform.WebUI/Pages/Services.razor.cs
@@ -3,11 +3,16 @@ using SchedulePlatform.Models.Entities;
 using Newtonsoft.Json;
 using System.Text.Json.Serialization;
 using System.Net.Http.Json;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
 
 namespace SchedulePlatform.WebUI.Pages
 {
     public partial class Services
     {
+        [Parameter]
+        public Guid Id { get; set; }
+
         private List<ServiceProvided> services = new List<ServiceProvided>();
 
         protected override async Task OnInitializedAsync()
@@ -19,5 +24,7 @@ namespace SchedulePlatform.WebUI.Pages
 
         }
     }
+
+    
 }
 
