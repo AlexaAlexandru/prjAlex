4cef6de282d56d441498e390a9aedb9d3837eec8
alexandrualexa <liv06ale@gmail.com>
2023-04-06 20:09:08 +0300

    Adjusting Dynamic Service and Service Detail Page

---
+++ SchedulePlatform.Shared/Models/ApiResult.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text.Json.Serialization;
+
+namespace SchedulePlatform.Shared.Models
+{
+    public class ApiResult<T>
+    {
+        public ApiResult() { }
+
+        [JsonConstructor]
+        private ApiResult(bool succeeded, T result, IEnumerable<string> errors)
+        {
+            Succeeded = succeeded;
+            Result = result;
+            Errors = errors;
+        }
+        public bool Succeeded { get; set; }
+        public T Result { get; set; }
+        public IEnumerable<string> Errors { get; set; }
+
+        public static ApiResult<T> Success(T result)
+        {
+            return new ApiResult<T>(true, result, new List<string>());
+        }
+        public static ApiResult<T> Failure(IEnumerable<string> errors)
+        {
+            return new ApiResult<T>(false, default, errors);
+        }
+    }
+}
+
--- SchedulePlatform.WebUI/Pages/ServiceDetail.razor.cs
+++ SchedulePlatform.WebUI/Pages/ServiceDetail.razor.cs
@@ -1,11 +1,36 @@
 ﻿using System;
-using SchedulePlatform.Models.Entities;
-using Newtonsoft.Json;
-using System.Text.Json.Serialization;
 using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
+using SchedulePlatform.Shared.Models;
+using System.Net.Http.Json;
 
 namespace SchedulePlatform.WebUI.Pages
 {
+    public partial class ServiceDetail
+    {
+        [Parameter]
+        public Guid Id { get; set; }
+        private ServiceResponse Service = new ServiceResponse();
 
+        protected async override Task OnParametersSetAsync()
+        {
+            var response = await HttpClient.GetFromJsonAsync<ApiResult<ServiceResponse>>($"api/ServiceProvided/{Id}");
+
+            Service = new ServiceResponse()
+            {
+                NameServiceProvided = response.Result.NameServiceProvided,
+                Description = response.Result.Description,
+                NutritionistService = response.Result.NutritionistService,
+                Price = response.Result.Price,
+                Type = response.Result.Type,
+                UrlPicture = response.Result.UrlPicture
+            };
+        }
+
+
+        void Navigate()
+        {
+            _nav.NavigateTo("/Appointment");
+        }
+    }
 }
+
